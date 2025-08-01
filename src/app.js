9dd4dc5777b0fe2215275107e2ad037ac87bf69d
Shreya Aggarwal <shreyaagg2038@gmail.com>
2025-08-01 22:27:49 +0530

    Added scripts in package.json and exploring GET,POST,PUT,PATCH,DELETE HTTP methods

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res)=>{
-    res.send("Hello from the server.")
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Shreya",lastName:"Aggarwal"});
+})
+app.post("/user",(req,res)=>{
+    res.send("Data saved successfully to DB");
+})
+app.put("/user",(req,res)=>{
+    res.send("Data updated successfully to DB");
+})
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted successfully from DB");
+})
+app.patch("/user",(req,res)=>{
+    res.send("Data patch modified successfully to DB");
+})
+app.use("/admin",(req,res)=>{
+    res.send("Hello from the admin.")
 })
 
 app.listen(3000,()=>{
