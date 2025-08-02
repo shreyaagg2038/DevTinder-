4b13bbe2508e66883b6ecccc3d99d82c79ef18b1
Shreya Aggarwal <shreyaagg2038@gmail.com>
2025-08-03 00:37:35 +0530

    Basci REST API's implemented

--- src/app.js
+++ src/app.js
@@ -5,6 +5,84 @@ const {connectDb} = require("./config/database");
 const {User} = require("./models/user");
 
 app.use(express.json());
+
+app.get("/user",async (req,res)=>{
+    try{
+    const filter = req.body.id;
+    //console.log(filter);
+    const users = await User.findById(filter);
+    if(!users){
+        res.status(404).send("User Not Found");
+    }
+    else{
+    res.send(users);
+    }
+
+    }
+    catch(err){
+        res.status(500).send(err.message);
+    }
+})
+
+app.delete("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const users  = await User.findByIdAndDelete(userId);
+        //console.log(users);
+        res.send("User deleted successfully");
+    }
+    catch(err){
+        res.status(500).send("User not deleted");
+    }
+})
+
+app.patch("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    const update = req.body;
+    console.log(userId);
+    console.log(update);
+    try{
+        const users  = await User.findByIdAndUpdate(userId,update,{returnDocument:"after"});
+        console.log(users);
+        res.send("User Name updated successfully");
+    }
+    catch(err){
+        res.status(500).send("User not deleted");
+    }
+})
+
+// app.put("/user",async(req,res)=>{
+//     const userId = req.body.userId;
+//     const update = req.body;
+//     console.log(userId);
+//     console.log(update);
+//     try{
+//         const users  = await User.findByIdAndUpdate(userId,update,{returnDocument:"after"});
+//         console.log(users);
+//         res.send("User Name updated successfully");
+//     }
+//     catch(err){
+//         res.status(500).send("User not deleted");
+//     }
+// })
+
+app.get("/feed",async (req,res)=>{
+    try{
+    //const filter = req.body.email;
+    //console.log(filter);
+    const users = await User.find({});
+    if(users.length===0){
+        res.status(404).send("User Not Found");
+    }
+    else{
+    res.send(users);
+    }
+
+    }
+    catch(err){
+        res.status(500).send("Something went wrong");
+    }
+})
 app.post("/signup",async (req,res)=>{
     const userObj = req.body;
     console.log(userObj);
