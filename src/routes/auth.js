172244cdebb52cf3f86afd2c28771503269fc790
Shreya Aggarwal <shreyaagg2038@gmail.com>
2025-08-05 21:30:18 +0530

    LOGOUT API added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -47,5 +47,10 @@ authRouter.post("/login",async (req,res)=>{
     }
 })
 
+authRouter.post('/logout',(req,res)=>{
+    res.cookie("token",null,{ expires: new Date(Date.now()) });
+    res.send("Logout Successfully");
+})
+
 
 module.exports = authRouter;
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,7 +3,7 @@ const profileRouter = express.Router();
 const {User} = require("../models/user");
 const {userAuth} = require("../middlewares/auth");
 
-profileRouter.get("/profile",userAuth,async (req,res)=>{
+profileRouter.get("/profile/view",userAuth,async (req,res)=>{
     try{
         const user = req.user;
         res.send(user);
@@ -12,5 +12,13 @@ profileRouter.get("/profile",userAuth,async (req,res)=>{
         res.status(500).send("ERROR: "+err.message);
     }
 })
+
+// profileRouter.patch('/profile/edit',userAuth,async (req,res)=>{
+//     console.log(req.body);
+//     const keyName = Object.keys(req.body).every(field=> console.log(field));
+//     res.send("Hey You are editing your profile");
+
+
+// })
 
 module.exports = profileRouter;
